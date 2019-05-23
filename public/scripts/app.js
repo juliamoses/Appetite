a00fd384ba030c906ec27c7256950fdbc313c779
Julia Moses <jslmoses@gmail.com>
2019-05-23 15:09:58 -0400

    ajax post made to populate checkout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,4 @@
 $(() => {
-<<<<<<< HEAD
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(items of users) {
-      $("<div>").text(items.name).appendTo($("body"));
-      $("<div>").text(items.price/100).appendTo($("body"));
-    }
-  });
-=======
   // $.ajax({
   //   method: "GET",
   //   url: "/api/users"
@@ -18,7 +7,24 @@ $(() => {
   //     $("<div>").text(user.name).appendTo($("body"));
   //   }
   // });;
->>>>>>> feature/ejs
 });
 
 
+
+const addCart = () => {
+  $('form').on('click', function(event) {
+    event.preventDefault()
+  
+
+      //on click of add to cart, populates
+      $.ajax({
+        type: 'POST',
+        url: '/checkout',
+        data: $(this).serialize(),
+      }).done(function() {
+		  $('texadd-cart');
+		  checkout();
+      })
+    }
+  })
+}
