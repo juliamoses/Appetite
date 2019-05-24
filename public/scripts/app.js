644b1602f567be1377ccece01f49b05f1155041c
Brian Murray <brianmurraysound@gmail.com>
2019-05-24 00:30:53 +0000

    items table used to populate orders page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,11 +16,41 @@ const itemsData = () => $.ajax({
 	type: 'GET',
 	url: '/api/items',
 	dataType: 'json'
-}).done(function (response) {
-  console.log("response: ", response);
+}).done(function (data) {
+  
+  renderMenuItems(data);
+  console.log("response: ", data);
 })
 itemsData();
 
+function renderMenuItems(items) {
+  var i = 0;
+  for (item of items ) {
+    console.log("item ", item);
+    let $items = createItemElement(item);
+
+    if (i < 4) {
+      $('.card-row1').append($items);
+    } else {
+      $('.card-row2').append($items);
+    }
+    i++;
+  }
+}
+
+function createItemElement(item) {
+  return `
+  <div class="card">
+    <img class="card-img-top" src="../images/pasta3.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="">${item.name}</h5>
+      <p class="card-text">Short desc of items</p>
+      <p class="prices">$${(item.price/100).toFixed(2)}</p>
+      <button class="btn btn-primary btn-lg add-cart" type="submit">Add to Cart</button>
+    </div>
+  </div>
+  `
+}
 
 
 // const addCart = () => {
