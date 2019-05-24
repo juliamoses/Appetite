c27d788f895d067ebf488fd00a920e61d6194de9
Brian Murray <brianmurraysound@gmail.com>
2019-05-24 00:39:11 +0000

    font on menu changed

--- db/seeds/items.js
+++ db/seeds/items.js
@@ -2,13 +2,15 @@ exports.seed = function(knex, Promise) {
   return knex('items').del()
     .then(function () {
       return Promise.all([
-        knex('items').insert({name: 'gnocco frito', price: 1000}),
-        knex('items').insert({name: 'branzino', price: 4200}),
-        knex('items').insert({name: 'tagliatelle', price: 3400}),
-        knex('items').insert({name: 'pizza funghi', price: 2800}),
-        knex('items').insert({name: 'crudo carne salada', price: 2200}),
-        knex('items').insert({name: 'negroni', price: 1200}),
-        knex('items').insert({name: 'grenach', price: 9000})
+        knex('items').insert({name: 'GNOCCO FRITO', price: 1000}),
+        knex('items').insert({name: 'BRANZINO', price: 4200}),
+        knex('items').insert({name: 'TAGLIATELLE', price: 3400}),
+        knex('items').insert({name: 'PIZZA FUNGHI', price: 2800}),
+        knex('items').insert({name: 'CRUDO CARNE SALADA', price: 2200}),
+        knex('items').insert({name: 'NEGRONI', price: 1200}),
+        knex('items').insert({name: 'GRENACHE', price: 9000}),
+        knex('items').insert({name: 'CARPACCIO', price: 1800}),
+
       ]);
     });
 };
